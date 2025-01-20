60a68186dc5e236f2e6d2a080639ebe9e82b2ea2
Siya016 <siya.ag07@gmail.com>
2025-01-20 16:12:55 +0530

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+// components/Navbar.tsx
+import React from 'react';
+import './Navbar.css'; // If you want to style it in a separate file
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Sticky Navbar</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
